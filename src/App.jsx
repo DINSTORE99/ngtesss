1753fdf94c834851fbc293425fe134239619e40f
DINSTORE99 <dinstoree41@gmail.com>
2026-08-15 22:40:35 +0700

    Update TURNSTILE_SITE_KEY to new value

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./style.css";
 ========================================================= */
 
 const TURNSTILE_SITE_KEY =
-  import.meta.env.VITE_TURNSTILE_SITE_KEY || "0x4AAAAAAEQ0naw15xO5xJRM";
+  import.meta.env.VITE_TURNSTILE_SITE_KEY || "0x4AAAAAAEQ1kOnV5CUtC";
 
 /* =========================================================
    API DATA
